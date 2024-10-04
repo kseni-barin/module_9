a0a674678df11a7184356188cfea8c8a3f886e9b
kseni-barin <kmalyutkina@yandex.ru>
2024-10-04 21:11:02 +0300

    Update module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,8 +1,6 @@
 def all_variants(text):
     for i in range(len(text)):
         for j in range(i, len(text)):
-        #for j in range(len(text) - i):
-            #yield text[j:j+i+1]
             yield text[i:j+1]
 
 a = all_variants("abc")
